f5fc2ddbdb5cd6477ab0fe6cc42d51490165e85b
TigistShiferaw <tgg909441@gmail.com>
2023-07-05 09:27:48 +0300

    delete build

--- build/helpers/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (execution) => (req, res, next) => {
-    execution(req, res, next).catch(next);
-};
-//# sourceMappingURL=asyncHandler.js.map
