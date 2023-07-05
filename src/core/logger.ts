9b879b4396bcc02f01744a943ed3bbf69fc7aa4b
Fitsum <fitsesile@gmail.com>
2023-07-05 13:23:41 +0300

    logging file changes

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,35 +1,7 @@
 import { createLogger, transports, format } from 'winston';
-import fs from 'fs';
-import path from 'path';
-import DailyRotateFile from 'winston-daily-rotate-file';
-import { environment, logDirectory } from '../config';
-
-let dir = logDirectory;
-if (!dir) dir = path.resolve('logs');
-
-// create directory if it is not present
-if (!fs.existsSync(dir)) {
-  // Create the directory if it does not exist
-  fs.mkdirSync(dir);
-}
+import { environment } from '../config';
 
 const logLevel = environment === 'development' ? 'debug' : 'warn';
-
-const dailyRotateFile = new DailyRotateFile({
-  level: logLevel,
-  // @ts-ignore
-  filename: dir + '/%DATE%.log',
-  datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  handleExceptions: true,
-  maxSize: '20m',
-  maxFiles: '14d',
-  format: format.combine(
-    format.errors({ stack: true }),
-    format.timestamp(),
-    format.json(),
-  ),
-});
 
 export default createLogger({
   transports: [
@@ -40,8 +12,15 @@ export default createLogger({
         format.prettyPrint(),
       ),
     }),
-    dailyRotateFile,
   ],
-  exceptionHandlers: [dailyRotateFile],
+  exceptionHandlers: [
+    new transports.Console({
+      level: 'error',
+      format: format.combine(
+        format.errors({ stack: true }),
+        format.prettyPrint(),
+      ),
+    }),
+  ],
   exitOnError: false, // do not exit on handled exceptions
 });
