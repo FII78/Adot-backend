d735a79230cd2e4adc67d9969f2d7a76eaaf0f06
Fitsum <fitsesile@gmail.com>
2023-07-05 13:45:05 +0300

    vercel configs

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(
 app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
 
 // Routes
-app.use('/', routes);
+app.use('/api', routes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(new NotFoundError()));
